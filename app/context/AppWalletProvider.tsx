89c13d3d47c7000df0bf13bb2e05a8ceabfbd966
Shrihari Kulkarni <shriharikulkarni@Shriharis-MacBook-Pro.local>
2024-10-17 15:44:08 +0530

    stake and unstake working well with decimal input

--- app/context/AppWalletProvider.tsx
+++ app/context/AppWalletProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
- 
+
 import React, { useMemo, useState } from "react";
 import {
   ConnectionProvider,
@@ -12,38 +12,48 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from "sonner";
 
 // import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
- 
+
 // Default styles that can be overridden by your app
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function AppWalletProvider({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(
-      () => [
-        // manually add any legacy wallet adapters here
-        // new UnsafeBurnerWalletAdapter(),
-      ],
-      [network],
-    );
-    const [queryClient] = useState(() => new QueryClient());
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [
+      // manually add any legacy wallet adapters here
+      // new UnsafeBurnerWalletAdapter(),
+    ],
+    [network],
+  );
+  const [queryClient] = useState(() => new QueryClient());
 
-   
-    return (
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider >
-              <QueryClientProvider client={queryClient}>
-              {children}
-              <Toaster />
-              </QueryClientProvider>
-             </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
-    );
-  }
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider >
+          <QueryClientProvider client={queryClient}>
+            {children}
+            <Toaster
+              position="bottom-left"
+              toastOptions={{
+                unstyled: true,
+                classNames: {
+                  toast: 'flex items-center   bg-green rounded-sm p-3  bg-card',
+                  title: 'font-semibold pr-4',
+                  description: 'text-sm ',
+                  icon: 'flex items-center justify-center size-10',
+                },
+              }} />
+          </QueryClientProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
